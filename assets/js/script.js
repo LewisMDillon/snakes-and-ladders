a31340695005eeb73ce480b185c912d2deb6e715
LewisMDillon <lewismdillon1@gmail.com>
2023-03-30 11:51:54 +0000

    Fix player pieces position on grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,19 @@ function startGame() {
 
 
 function rollDice() {
-    let diceNum = Math.floor(Math.random() * 6) + 1
+    // let diceNum = Math.floor(Math.random() * 6) + 1
+    let diceRoller = document.getElementById('dice-roller')
 
 }
 
 function movePiece() {
     let playerPosition = playerPosition + diceNum
-
 }
 
 function resetGame() {
 
 }
+
+
+
+
